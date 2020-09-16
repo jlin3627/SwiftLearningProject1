58369ac4211bfb0060fa4a9f3c3aae4704a0fb32
jlin3627 <jlin3627@gmail.com>
2020-09-16 15:43:47 -0400

    Swift Meow test project
    Simple project to learn swift, a button with cat image and press the button for meow sound and meow display

--- Test1/ViewController.swift
+++ Test1/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-	override func viewDidLoad() {
+	@IBOutlet weak var lblTitle: UILabel!
+	@IBOutlet weak var btnTitle: UIButton!
+
+	var catSound: AVAudioPlayer = AVAudioPlayer()
+
+
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		btnTitle.imageView?.contentMode = .scaleAspectFit
+
+		let soundFile = Bundle.main.path(forResource: "Cat", ofType: "wav")
+
+		do
+		{
+			// ! <- tell us the file might be empty
+			try catSound = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundFile!))
+		}
+		catch
+		{
+			print(error)
+		}
 	}
 
+	@IBAction func btnTitleTouchUp(_ sender: Any)
+	{
+		lblTitle.isHidden = false;
 
+		catSound.play()
+
+		Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(hideLabel), userInfo: nil, repeats: false)
+	}
+
+	@objc func hideLabel()
+	{
+		lblTitle.isHidden = true
+	}
 }
 

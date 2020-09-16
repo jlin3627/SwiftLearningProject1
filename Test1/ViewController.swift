6512671eb3ce3620db71a863062b8fe90230b8a4
jlin3627 <jlin3627@gmail.com>
2020-09-16 14:07:08 -0400

    Initial Commit

---
+++ Test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by Jing Yee Lin on 9/16/20.
+//  Copyright © 2020 Jing Yee Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
